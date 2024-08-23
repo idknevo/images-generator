9d67908e03a506ce9d41503b7674894611fb83d2
idknevo <nevoexists@gmail.com>
2024-08-23 16:20:20 +0300

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+const imgsContainer = document.querySelector(".images-container");
+const btn = document.querySelector(".btn");
+
+const addNewImages = function (noOfImages) {
+  for (let i = 0; i < noOfImages; i++) {
+    const newImage = document.createElement("img");
+    const randomNumber = Math.ceil(Math.random() * 2000);
+    newImage.src = `https://picsum.photos/300?random=${randomNumber}`;
+    imgsContainer.append(newImage);
+  }
+};
+
+btn.addEventListener("click", () => {
+  addNewImages(10);
+});
